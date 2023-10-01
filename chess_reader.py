94cdb156875c644f2687970ac0fc38d9f4beec00
Marcus <marcus@lastcraft.com>
2023-10-01 16:51:36 +0100

    Factor our code duplication

--- chess_reader.py
+++ chess_reader.py
@@ -3,7 +3,15 @@ import time
 import gtts
 from playsound import playsound
 
-fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+START_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+PIECES = {
+    "r": "Rook",
+    "b": "Bishop",
+    "n": "Knight",
+    "q": "Queen",
+    "k": "King",
+    "p": "Pawn"
+}
 
 def letter(num):
     return "abcdefgh"[num - 1]
@@ -11,10 +19,11 @@ def letter(num):
 def phoneme(num):
     return ["ay", "b",  "c", "d", "e", "f",  "g", "h"][num - 1]
 
-def colour(piece):
-    if piece.islower():
-        return "black"
-    return "white"
+def colour(character):
+    return "black" if character.islower() else "white"
+
+def piece(character):
+    return PIECES[character.lower()]
 
 def say(text):
     tts = gtts.gTTS(text)
@@ -23,32 +32,17 @@ def say(text):
     os.remove("./speech.mp3")
 
 
-def main():
+def main(position):
     coords = [1, 8]
-    for character in fen:
+    for character in position:
         if character == "/":
             coords[0] = 1
             coords[1] -= 1
+        elif character in list(PIECES.keys()) + list(map(lambda x: x.upper(), PIECES.keys())):
+            say(colour(character) + " " + piece(character) + " " + phoneme(coords[0]) + " " + str(coords[1]))
+            coords[0] += 1
         elif character.isdigit():
             coords[0] += int(character)
-        elif character in "kK":
-            say(colour(character) + " king " + phoneme(coords[0]) + " " + str(coords[1]))
-            coords[0] += 1
-        elif character in "qQ":
-            say(colour(character) + " queen " + phoneme(coords[0]) + " " + str(coords[1]))
-            coords[0] += 1
-        elif character in "rR":
-            say(colour(character) + " rook " + phoneme(coords[0]) + " " + str(coords[1]))
-            coords[0] += 1
-        elif character in "bB":
-            say(colour(character) + " bishop " + phoneme(coords[0]) + " " + str(coords[1]))
-            coords[0] += 1
-        elif character in "nN":
-            say(colour(character) + " knight " + phoneme(coords[0]) + " " + str(coords[1]))
-            coords[0] += 1
-        elif character in "pP":
-            say(colour(character) + " pawn " + phoneme(coords[0]) + " " + str(coords[1]))
-            coords[0] += 1
         time.sleep(1)
 
-main()
+main(START_POSITION)
